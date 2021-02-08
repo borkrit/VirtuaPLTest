3fb0d5e68cbd90f2cd124eadde6d2182f22daab9
vlad pohorilyi <vladyslav.pohorilyi@gmail.com>
2021-02-08 22:12:24 +0100

    task 3 done

--- src/js/index.js
+++ src/js/index.js
@@ -4,15 +4,38 @@ const checkbox = document.getElementById('checkbox');
 const btnSubmit = document.getElementById('btn-submit');
 const btnCloseForm = document.getElementById('close');
 const form = document.getElementById('form-login');
+const btnOpenForm = document.getElementById('show-popup-form');
+const pop = document.getElementById('popup');
+const success = document.getElementById('success');
+
+
+
+btnOpenForm.addEventListener('click',()=>{
+    pop.removeAttribute('hidden');
+    btnOpenForm.setAttribute('hidden',true)
+})
 
 btnCloseForm.addEventListener('click',()=>{
-    form.style.display = 'none';
+    pop.setAttribute('hidden',true)
+    btnOpenForm.removeAttribute('hidden')
 })
 
 
 function onClickChangeChekBox (){
     checkbox.checked ? btnSubmit.removeAttribute('disabled') : btnSubmit.setAttribute('disabled',true)
-
 }
 
+
+form.onsubmit = submit
+
+
+function submit (event){
+    pop.style.cursor='wait';
+   setTimeout(()=>{
+    form.setAttribute('hidden','');
+    success.removeAttribute('hidden');
+   },3000)
+   
+   event.preventDefault();
+}
 

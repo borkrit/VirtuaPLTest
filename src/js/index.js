9e34f9b3d818d7cec7b2b0ba6df31681783f309a
vlad pohorilyi <vladyslav.pohorilyi@gmail.com>
2021-02-08 17:23:06 +0100

    task-1/task-2 done

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
 /* Here goes your JS code */
+
+const checkbox = document.getElementById('checkbox');
+const btnSubmit = document.getElementById('btn-submit');
+const btnCloseForm = document.getElementById('close');
+const form = document.getElementById('form-login');
+
+btnCloseForm.addEventListener('click',()=>{
+    form.style.display = 'none';
+})
+
+
+function onClickChangeChekBox (){
+    checkbox.checked ? btnSubmit.removeAttribute('disabled') : btnSubmit.setAttribute('disabled',true)
+
+}
+
+
